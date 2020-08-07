147dc89265178359c344ec820cf6487f611303d7
jmiguel-devs <69360928+jmiguel-devs@users.noreply.github.com>
2020-08-07 20:57:43 +0200

    Add files via upload
    Creados los principales elementos. Funcionalidad completa, próxima actualización.

---
+++ index.js
@@ -0,0 +1,111 @@
+
+var userPoints = 0;
+var computerPoints= 0;
+const optionsGameArray = ['Rock', 'Paper', 'Scissors'];
+const rockBtn = document.getElementById('Rock');
+const paperBtn = document.getElementById('Paper');
+const scissorsBtn = document.getElementById('Scissors');
+const userResult = document.getElementById('user-points');
+const computerResult = document.getElementById('computer-points');
+const modalElement = document.getElementById('exampleModalLong');
+
+function computerPlay(){
+    let randomOptionGame = Math.floor(Math.random() * optionsGameArray.length);
+    let computerChoice = optionsGameArray[randomOptionGame];
+    return computerChoice;
+}
+
+function btnHandler(){
+    if(userPoints < 5 || computerPoints < 5){
+        rockBtn.addEventListener("click", () => {
+            game('Rock');
+        });
+        
+        paperBtn.addEventListener("click", () => {
+            game('Paper');
+        });
+        
+        scissorsBtn.addEventListener("click", () => {
+            game('Scissors');
+        });
+
+    } 
+}
+
+function userWins(){
+    userPoints++,
+    userResult.textContent = userPoints;
+    if(userPoints == 5){
+        computerResult.textContent = '0';
+        userResult.textContent = '0';
+        restartGame();
+        $('#userWinsModal').modal('show');
+    }
+}
+
+function computerWins(){
+    computerPoints++,
+    computerResult.textContent = computerPoints;
+    if(computerPoints == 5){
+        computerResult.textContent = '0';
+        userResult.textContent = '0';
+        restartGame();
+        $('#computerWinsModal').modal('show');
+    }
+}
+
+function singleRound(playerChoice, computerChoice){
+    //rock options
+    if(playerChoice === 'Rock' && computerChoice === 'Rock'){
+        console.log('It´s a tie')
+    } else if(playerChoice === 'Rock' && computerChoice === 'Paper'){
+        console.log('You select Rock and IA selects Paper' + '\nYou WIN!');
+        userWins();
+
+    } else if(playerChoice === 'Rock' && computerChoice === 'Scissors'){
+        console.log('You select Rock and IA selects Scissors' + '\nAI WIN!');
+        computerWins();
+    }
+
+    // paper options
+    if(playerChoice === 'Paper' && computerChoice === 'Paper'){
+        console.log('It´s a tie')
+    } else if(playerChoice === 'Paper' && computerChoice === 'Rock'){
+        console.log('You select Paper and IA selects Rock' + '\nYou WIN!');
+        userWins();
+    } else if(playerChoice === 'Paper' && computerChoice === 'Scissors'){
+        console.log('You select Paper and IA selects Scissors' + '\nAI WIN!');
+        computerWins();
+    }
+
+    // scissors options
+    if(playerChoice === 'Scissors' && computerChoice === 'Scissors'){
+        console.log('It´s a tie')
+    } else if(playerChoice === 'Scissors' && computerChoice === 'Paper'){
+        console.log('You select Scissors and IA selects Paper' + '\nYou WIN!');
+        userWins();
+    } else if(playerChoice === 'Scissors' && computerChoice === 'Rock'){
+        console.log('You select Scissors and IA selects Rock' + '\nAI WIN!');
+        computerWins();
+    }
+
+}
+
+function game(playerChoice){
+    singleRound(playerChoice, computerPlay());
+    if(userPoints == 5){
+        console.log('golaso');
+        
+    } else if(computerPoints == 5){
+        console.log('perdistes');
+       
+    }
+}
+
+function restartGame(){
+    userPoints = 0;
+    computerPoints = 0;
+}
+
+
+btnHandler();
